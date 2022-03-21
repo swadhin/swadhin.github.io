9fb7f5020708fbfb2fca9de1e35f2ee01266c9eb
Swadhin Pradhan <swadhin.pradhan@meraki.net>
2022-03-21 15:27:22 -0500

    initial files

---
+++ upload_file.php
@@ -0,0 +1,33 @@
+<?php
+$allowedExts = array("txt", "rtf");
+$temp = explode(".", $_FILES["file"]["name"]);
+$extension = end($temp);
+
+if ( ($_FILES["file"]["size"] < 90000)
+    && in_array($extension, $allowedExts) ) {
+          if ($_FILES["file"]["error"] > 0) {
+                  echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
+                    } else {
+                            echo "Upload: " . $_FILES["file"]["name"] . "<br>";
+                            echo "Type: " . $_FILES["file"]["type"] . "<br>";
+                                echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+                                    echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
+                                    if (file_exists("upload/" . $_FILES["file"]["name"])) {
+                                              echo $_FILES["file"]["name"] . " already exists. ";
+                                                  } else {
+                                                            move_uploaded_file($_FILES["file"]["tmp_name"],
+                                                                  "upload/" . $_FILES["file"]["name"]);
+                                                                  echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
+                                                                      }
+                                      }
+    } else {
+          echo "Invalid file";
+    }
+?>
+    }
+                                                  }
+                                    }
+                    }
+          }
+    }>))>
+
